8f6e07e9d3dbad29583658cb2ef0ee900d70110c
ghawken <ghawken@hotkey.net.au>
2018-02-11 19:01:45 +1100

    0.6.5 Final Change for accuracy of Geofence Entry/Exit

--- iFindFriendsMini.indigoPlugin/Contents/Server Plugin/plugin.py
+++ iFindFriendsMini.indigoPlugin/Contents/Server Plugin/plugin.py
@@ -911,7 +911,7 @@ class Plugin(indigo.PluginBase):
                                 # this may be the space to look at more complication accuracy versus seperation type calculation
                                 # will run and gather more data first
                                 # change here - not accurate do not remove rom Geofence
-                                if iDevUniqueName in geoDevices.states['listFriends'] and DistanceAccurate > igeoRangeDistance:  #if already present ignore accuracy data
+                                if iDevUniqueName in geoDevices.states['listFriends'] and DistanceAccurate <= igeoRangeDistance:  #if already present ignore accuracy data
                                     self.logger.debug(u'---------------- Accuracy Poor: ' + unicode(iDevUniqueName) + ' & Is WITHIN Geofence:' + unicode(igeoName) + ', poor accuracy so do not remove.  Distance:'+unicode(iSeparationABS)+ u'     -- Not Used -- DistanceAccurate Result equals:'+unicode(DistanceAccurate))
                                     if self.debuggeofence:
                                         self.newlogger.info(u"{0:=^160}".format(""))
